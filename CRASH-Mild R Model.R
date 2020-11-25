ecbe5b404de6810c1697fe896dd46c3f09b0f3b7
Jack Williams <jack.williams@lshtm.ac.uk>
2020-11-25 17:17:10 +0000

    CRASH Mild model start

---
+++ CRASH-Mild R Model.R
@@ -0,0 +1,20 @@
+
+## CRASH-Mild - Economic model ## 
+
+
+
+
+treatment.effect <- 0.667
+
+treatment.effect.lci <- 0.411
+treatment.effect.uci <- 1.083
+
+hi.risk <- 9/920
+non.hi.risk <- 13/440
+
+age <- 57.7
+
+acm <- 1
+
+getwd()
+
