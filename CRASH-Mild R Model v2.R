25b06e7692781a0c62811c96c40771f4a5a90fd5
Jack Williams <jack.williams@lshtm.ac.uk>
2021-04-29 15:52:48 +0100

    Adding EVPPI for trial, removing EVSI work

--- CRASH-Mild R Model v2.R
+++ CRASH-Mild R Model v2.R
@@ -24,7 +24,7 @@ age <- 70
 male = 0.5 # plaecholder
 time.horizon = min(60, 100-ceiling(age))
 
-
+lambda <- seq(from = 0, to = 40000, by = 100)
 
 ## Age trace
 
@@ -318,7 +318,6 @@ effective.population <- 1
 ##  TRACE CALCULATIONS AND OUTCOMES  ## 
 
 
-
 run.model <- function(clinical = clin.char, dis.plac = disability.placebo, dis.txa = disability.txa, util.values = utility, cost = costs, 
                       dec = utility.decrement, discount.c = disc.c, discount.o = disc.o) {
   
@@ -482,21 +481,20 @@ for(p in 1:sims){
 
 # Generate CEAC table
 
-gen.ceac.table <- function(results, lambda.inc = 500){
+gen.ceac.table <- function(results, lam = lambda){
   
-  lambda <- seq(from = 0, to = 40000, by = lambda.inc)
-    
+  
   ## CEAC ## 
   
-  lambda.table <- matrix(lambda, ncol = length(lambda), nrow = dim(results)[1], byrow = TRUE) 
+  lambda.table <- matrix(lam, ncol = length(lam), nrow = dim(results)[1], byrow = TRUE) 
   inmb.count <- ((results[,4] * lambda.table) - results[,3]) - ((results[,2] * lambda.table) - results[,1]) > 0 
   
   prob.ce <- apply(inmb.count, 2, mean) 
-  ceac.table <- data.frame(lambda, prob.ce) 
+  ceac.table <- data.frame(lam, prob.ce) 
   
   ## EVPI ## 
   
-  evpi.table <- matrix(lambda, ncol = length(lambda), nrow = dim(results)[1], byrow = TRUE) 
+  evpi.table <- matrix(lam, ncol = length(lam), nrow = dim(results)[1], byrow = TRUE) 
 
   nmb.p <- ((results[,2] * evpi.table) - results[,1])  
   nmb.t <- ((results[,4] * evpi.table) - results[,3]) 
@@ -514,7 +512,7 @@ gen.ceac.table <- function(results, lambda.inc = 500){
   evpi.mat[evpi.mat<0] <- 0
   evpi.m <- apply(evpi.mat, 2, mean)
   
-  evpi <- data.frame(lambda, evpi.m)
+  evpi <- data.frame(lam, evpi.m)
 
   return(list(ceac.table,
               evpi))
@@ -522,10 +520,10 @@ gen.ceac.table <- function(results, lambda.inc = 500){
 }
 
 
-ceac <- gen.ceac.table(psa.results, 100)[[1]]
-evpi <- gen.ceac.table(psa.results, 100)[[2]]
+ceac <- gen.ceac.table(psa.results)[[1]]
+evpi <- gen.ceac.table(psa.results)[[2]]
 
-evpi.pop <- gen.ceac * effective.population
+evpi.pop <- evpi[,2] * effective.population
 
 # Graphics  - TBC (take from other sources)
 
@@ -592,20 +590,27 @@ gen.evpi.graph(evpi)
 # inner.loops <- 300
 # outer.loops <- 300
 
-# Select lambda values to be considered 
-lambda <- seq(from = 0, to = 30000, by = 500)
-
-
 # Sample all probabilistic parameters
 
-clin.char.sims <- gen.clinical.characteristics()[[2]]
-disability.placebo.sims <- gen.clinical.characteristics()[[5]]
-disability.txa.sims <- disability.placebo.sims # same as placebo (equal for both arms)
-#disability.txa.sims <- gen.clinical.characteristics()[[6]]
-utility.sims <- gen.utility.sims()
-costs.sims <- gen.costs(disability.placebo, disability.txa, disability.placebo.sims, disability.txa.sims)[[2]]
+# clin.char.sims <- gen.clinical.characteristics()[[2]]
+# disability.placebo.sims <- gen.clinical.characteristics()[[5]]
+# disability.txa.sims <- disability.placebo.sims # same as placebo (equal for both arms)
+# #disability.txa.sims <- gen.clinical.characteristics()[[6]]
+# utility.sims <- gen.utility.sims()
+# costs.sims <- gen.costs(disability.placebo, disability.txa, disability.placebo.sims, disability.txa.sims)[[2]]
 
 
+
+# Generate matrices for EVPPI results to be stored in
+
+inner.results <- matrix(0, inner.loops, 4)
+
+evppi.results.placebo <- matrix(0, ncol = length(lambda), nrow = outer.loops)
+colnames(evppi.results.placebo) <- as.character(lambda)
+evppi.results.txa <- evppi.results.placebo
+
+
+# EVPPI functions
 
 gen.nmb <- function(results, lam = lambda){
   
@@ -625,17 +630,6 @@ gen.nmb <- function(results, lam = lambda){
   return(list(nmb.t, nmb.p))
   
 }
-
-
-# Generate matrices for EVPPI results to be stored in
-
-inner.results <- matrix(0, inner.loops, 4)
-
-evppi.results.placebo <- matrix(0, ncol = length(lambda), nrow = outer.loops)
-colnames(evppi.results.placebo) <- as.character(lambda)
-evppi.results.txa <- evppi.results.placebo
-
-# EVPPI functions
 
 gen.evppi.results <- function(evppi.results1 = evppi.results.placebo, evppi.results2 = evppi.results.txa, lam = lambda){
   
@@ -664,21 +658,21 @@ gen.evppi.results <- function(evppi.results1 = evppi.results.placebo, evppi.resu
 
 ## EVPPI Loops - 'Double Monte Carlo loop method' 
 
-
 ## EVPPI loops - Head injury and TXA treatment effect
-
 for(a in 1:outer.loops){
 
 ## 1. Select the 'partial' parameter from the outer loop 
-clin.sim <- unlist(clin.char.sims[a,])
 
+clin.sim <- unlist(clin.char.sims[a,])
+  
   for(b in 1:inner.loops){
   
   # Select traditional parameters, minus the outer loop parameter
 
+  
   clin.sim[4:5] <- unlist(clin.char.sims[b,4:5]) # Keep SMRs in PSA 
-  dis.placebo.sim <- unlist(disability.placebo.sims[p,])
-  dis.txa.sim <- unlist(disability.txa.sims[p,])
+  dis.placebo.sim <- unlist(disability.placebo.sims[b,])
+  dis.txa.sim <- unlist(disability.txa.sims[b,])
   utility.sim <- unlist(utility.sims[b,])
   cost.sim <- unlist(costs.sims[b,])
   
@@ -691,14 +685,10 @@ clin.sim <- unlist(clin.char.sims[a,])
   evppi.results.txa[a,] <- nmb[[2]]
   
 }
-
 evppi.head.injury <- gen.evppi.results(evppi.results.placebo, evppi.results.txa, lambda)
 
 
-
-
 ## EVPPI loops - Head injury and TXA treatment effect
-
 for(a in 1:outer.loops){
   
   ## 1. Select the 'partial' parameter from the outer loop 
@@ -725,14 +715,10 @@ for(a in 1:outer.loops){
   evppi.results.txa[a,] <- nmb[[2]]
   
 }
-
 evppi.smr <- gen.evppi.results(evppi.results.placebo, evppi.results.txa, lambda)
 
 
-
-
 ## EVPPI loops - Disability / outcomes   
-
 for(a in 1:outer.loops){
   
   ## 1. Select the 'partial' parameter from the outer loop 
@@ -758,17 +744,10 @@ for(a in 1:outer.loops){
   evppi.results.txa[a,] <- nmb[[2]]
   
 }
-
 evppi.disability <- gen.evppi.results(evppi.results.placebo, evppi.results.txa, lambda)
 
 
-
-
-
-
-
 ## EVPPI loops - Utility  
-
 for(a in 1:outer.loops){
   
   ## 1. Select the 'partial' parameter from the outer loop 
@@ -793,14 +772,10 @@ for(a in 1:outer.loops){
   evppi.results.txa[a,] <- nmb[[2]]
   
 }
-
 evppi.utility <- gen.evppi.results(evppi.results.placebo, evppi.results.txa, lambda)
 
 
-
-
 ## EVPPI loops - Costs  
-
 for(a in 1:outer.loops){
   
   ## 1. Select the 'partial' parameter from the outer loop 
@@ -826,12 +801,10 @@ for(a in 1:outer.loops){
   evppi.results.txa[a,] <- nmb[[2]]
   
 }
-
-# Calculate the EVPPI 
 evppi.costs <- gen.evppi.results(evppi.results.placebo, evppi.results.txa, lambda)
 
 
-## Reshaping? 
+## Reshaping and plotting ## 
 
 evppi.wide <- data.frame(evppi.head.injury,
                          evppi.smr[,2],
@@ -875,7 +848,69 @@ gen.evppi.graph(evppi.long)
 
 
 
+#### EVPPI for trial parameters 
 
+for(a in 1:outer.loops){
+  
+  ## 1. Select trial parameters
+  clin.sim[1:3] <- unlist(clin.char.sims[a,1:3]) # HI risk and tx effect
+  dis.placebo.sim <- unlist(disability.placebo.sims[a,])
+  dis.txa.sim <- unlist(disability.txa.sims[a,])  
+  
+  
+  for(b in 1:inner.loops){
+    
+    # Select traditional parameters, minus the outer loop parameter
+    clin.sim[4:5] <- unlist(clin.char.sims[b,4:5]) # Keep SMRs in PSA 
+    utility.sim <- unlist(utility.sims[b,]) ## Utility values
+    cost.sim[4:9] <- unlist(costs.sims[b,4:9])
+    
+    inner.results[b,] <- run.model(clin.sim, dis.placebo.sim, dis.txa.sim, utility.sim, cost.sim)[[1]] 
+  }
+  
+  #after each inner loop PSA, calculate the mean NMB for each tx and store the results
+  nmb <- gen.nmb(inner.results)
+  evppi.results.placebo[a,] <- nmb[[1]]
+  evppi.results.txa[a,] <- nmb[[2]]
+  
+}
+
+evppi.trial.parms <- gen.evppi.results(evppi.results.placebo, evppi.results.txa, lambda)
+
+## Reshape / plot
+
+evppi.trial <- cbind(evpi, evppi.trial.parms[,2])
+
+dim(evppi.trial)
+colnames(evppi.trial) <- c('lambda', 'EVPI', 'EVPPI for trial parameters')
+evppi.trial.long <- evppi.trial %>% gather(Parameters, VoI, 2:3)
+gen.evppi.graph(evppi.trial.long)
+
+
+gen.evppi.trial.graph = function(evppi, save = FALSE) {
+  
+  z = ggplot(evppi) + geom_line(aes(x=lambda, y=VoI, colour = Parameters, linetype = Parameters), size=0.6) + 
+    labs(x = "Willingness to pay (£)", text = element_text(size=4)) + 
+    labs(y = "EVPPI (£)", text = element_text(size=4)) + theme_classic() +
+    theme(legend.title = element_blank(), axis.title=element_text(face="bold"), 
+          axis.title.x = element_text(margin = margin(t = 7, r = 0, b = 3, l = 0)), 
+          axis.title.y = element_text(margin = margin(t = 0, r = 7, b = 0, l = 3)), 
+          panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+          legend.key.width=unit(1.8,"line"), text = element_text(size=7),
+          plot.margin=unit(c(1.2,0.5,0,1.2),"cm")) + 
+    scale_x_continuous(labels = scales::comma, breaks = c(seq(0,100000,5000)), limits = c(0,40000), expand = c(0, 0.1)) + 
+    scale_y_continuous(expand = c(0, 0)) + 
+    scale_linetype_manual(values=c("solid", "longdash")) + 
+    scale_color_manual(values=c("black", "black")) + 
+        geom_vline(xintercept = 20000, linetype="dotted", size=0.25)
+  
+  
+  if(save == TRUE) ggsave(paste("figures\\EVPPI-Trial",Sys.Date(),".png"), z, width=180, height=100, dpi=300, units='mm')
+  
+  return(z)  
+  
+}
+gen.evppi.trial.graph(evppi.trial.long)
 
 # ## EVSI - WOrk in progress (unsure if JAGS/OpenBUGS needed?) ##
 # 
