70a874d6498f2ba455f3e0579d5899497f2c3784
Jack <jack.williams@lshtm.ac.uk>
2021-06-23 13:48:59 +0100

    Adding ONS population projections

--- VoI Parameters.R
+++ VoI Parameters.R
@@ -2,6 +2,7 @@
 
 
 ons.pop <- read.csv("inputs/ONS_pop.csv", header=TRUE)
+ons.pop.proj <- read.csv("inputs/ons.pop.proj.csv", header = TRUE)
 colnames(ons.pop) <- c("age", "male", "female")
 
 # Settings and inputs for analysis
@@ -22,12 +23,23 @@ prop.mild <- 0.9
 # incidence <- data.frame(age = est.male[[1]], male = est.male[[2]], female = est.female[[2]]) * incidence.ratio 
 
 ## Alternative incidence - Tennent 2005
-
 incidence <- 410.8/100000
+
+# Population (fixed per year)
 population <- sum(subset(ons.pop, age >= age.range[1])) 
-total <- population * incidence * prop.mild
+
+# Population projection (ONS 2018 projections)
+# Estimate from ONS projections
+rownames(ons.pop.proj) <- ons.pop.proj[,1]
+ons.pop.proj[,1] <- seq(0, 100, 5)
+pop <- ons.pop.proj[,-c(2,3,4,5)] ## Removing 2018 to 2021
+pop.sums <- apply(subset(pop, Ages>=70), 2, sum) 
+population.vec <- as.numeric(pop.sums[2:(voi.th+1)] * 1000) # adjusting for per 1000 data
+
 
 ## Multiply UK age-adjusted incidence by population 
+total <- population.vec * incidence * prop.mild
+
 effective.population <- sum( total * (1/(1+discount)^(0:(voi.th-1))))
 effective.population
 
@@ -36,4 +48,6 @@ effective.population
 population.alt <- sum(subset(ons.pop, age >= age.range.alt[1])) 
 total.alt <- population.alt * incidence * prop.mild
 effective.population.alt <- sum( total.alt * (1/(1+discount)^(0:(voi.th-1))))
+
+
 
